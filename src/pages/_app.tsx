058f77746a5d68782732a4860bba7dba3ce455d8
softgenai <admin@softgen.ai>
2026-01-02 20:54:08 +0000

    feat(legal-acceptance): Integrate legal acceptance flow into application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import { Toaster } from "@/components/ui/toaster";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { LegalAcceptanceProvider, useLegalAcceptance } from "@/contexts/LegalAcceptanceContext";
+import { LegalAcceptanceModal } from "@/components/LegalAcceptanceModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+function AppContent({ Component, pageProps }: AppProps) {
+  const { showModal, acceptTerms } = useLegalAcceptance();
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <LegalAcceptanceModal open={showModal} onAccept={acceptTerms} />
+      <Toaster />
+    </>
+  );
+}
+
+export default function App(props: AppProps) {
   return (
     <LanguageProvider>
-      <Component {...pageProps} />
-      <Toaster />
+      <LegalAcceptanceProvider>
+        <AppContent {...props} />
+      </LegalAcceptanceProvider>
     </LanguageProvider>
   );
 }
